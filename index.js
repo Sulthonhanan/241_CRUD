c17d2fdff49f056589029b4764f7079886f2b605
Sulthonhanan <hananakhnaf31@gmail.com>
2025-10-21 10:10:06 +0700

    Tambah endpoint API mahasiswa

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+let mysql = require('mysql');
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -8,4 +11,31 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Sulthonhanan123',
+  database: 'biodata',
+  port: 3306
+});         
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the MySQL:', err);
+    return;
+  }
+    console.log('Connection Successfully.');
+});
+
+app.get('/api/mahasiswa', (req, res) => {
+  db.query('SELECT * FROM mahasiswa', (err, results) => {
+    if (err) {
+      console.error('Error executing users:', err);
+      res.status(500).send('Error executing users');
+      return;
+    }
+    res.json(results);
+  });
 });
